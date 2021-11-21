2ee2d8f98f40c02caa620a20bcf28b3ddbc9f60a
szabogtamas <szabogtamas@gmail.com>
2021-11-21 12:12:38 +0100

    Defined main function

--- scripts/py/netoglyc_parser.py
+++ scripts/py/netoglyc_parser.py
@@ -9,3 +9,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input_file")
 parser.add_argument("output_file")
 args = parser.parse_args()
+
+def parse_netoglyc(fn):
+    f = open(fn, "r")
+    entries = f.read()
+    f.close()
+    tabular = re.findall(r'(?<=\n)[A-Za-z0-9]+\s+[ST]\s+.*(?=\n)', entries)
+    tmp_data = StringIO("\n".join(tabular))
+    df = pd.read_fwf(tmp_data)
+    return df
